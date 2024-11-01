7ef58affe67510ce0b2d9ce77f434d98e5f25523
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-11-01 23:25:55 +0000

    Game object adds meteors to linked list on collision

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -11,6 +11,7 @@ public class CursorMovement : MonoBehaviour
     void Start()
     {
         cameraObj = GameObject.Find("Main Camera").GetComponent<Camera>(); 
+        ObjectsIntersecting = new LinkedList<GameObject>();
     }
 
     // Update is called once per frame
@@ -21,15 +22,47 @@ public class CursorMovement : MonoBehaviour
         Vector3 cursorPos = cameraObj.ScreenToWorldPoint(mousePos);
         cursorPos.z = 0;
         transform.position = cursorPos;
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collieded");
+        Debug.Log(ObjectsIntersecting.Count);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Triggered");
+        MeteorPhysics meteorComp = collision.gameObject.GetComponent<MeteorPhysics>();
+        if(meteorComp != null) 
+        {
+            //If game object has meteor component
+            foreach(GameObject g in ObjectsIntersecting)
+            {
+                //Check if meteor comp is already in list (won't be but good to check)
+                if(g.GetComponent<MeteorPhysics>()==meteorComp)
+                {
+                    Debug.Log("Something went wrong");
+                    return; //If more needs to happen after this, change return with something else to prevent adding
+                }
+            }
+
+            ObjectsIntersecting.AddLast(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Check if object is a meteor
+        if(other.gameObject.GetComponent<MeteorPhysics>()==null) 
+        {
+            //If more is added, change this return for something to prevent it running the foreach
+            return;
+        }
+        
+        foreach (GameObject g in ObjectsIntersecting)
+        {
+            //Ensure item is in list (no reason it wouldn't be)
+            if (g == other.gameObject)
+            {
+                ObjectsIntersecting.Remove(g);
+                return;
+            }
+        }
     }
 }
