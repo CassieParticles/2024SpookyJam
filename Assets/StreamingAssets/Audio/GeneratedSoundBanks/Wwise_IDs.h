2ef20a60c6efcbff00f9e21e39eba834d520ef1d
Rebecca <146950501+Azaersyss@users.noreply.github.com>
2024-11-02 01:09:58 +0000

    Added SFX/Soundbank
    Added the main soundbank, as well as a bunch of sfx events, including some random containers and set parameters for more dynamic audio.

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUTTON_CLICK = 814543256U;
+        static const AkUniqueID ENGINE_S2 = 2614347193U;
+        static const AkUniqueID ENGINE_S3 = 2614347192U;
+        static const AkUniqueID ENGINE_S4 = 2614347199U;
+        static const AkUniqueID METEOR_EXPLODE = 809529563U;
+        static const AkUniqueID METEOR_GRAB = 1149410026U;
+        static const AkUniqueID METEOR_THROW = 3809331500U;
+        static const AkUniqueID PLAYER_DAMAGED = 1386564838U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID TEMP_MUSIC = 1192912587U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
