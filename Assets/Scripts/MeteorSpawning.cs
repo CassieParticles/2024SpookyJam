46f8adbffd4dedf8bbc1124f6ed42e7907959139
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-11-02 00:12:19 +0000

    Added SpawnMeteor function

--- Assets/Scripts/MeteorSpawning.cs
+++ Assets/Scripts/MeteorSpawning.cs
@@ -6,11 +6,37 @@ using UnityEngine;
 
 public class MeteorSpawning : MonoBehaviour
 {
+    [SerializeField] private float spawnDistance = 10;   //Figure out the spawn distance
+    [SerializeField] private float randomOffsetRange = 20;
+    [SerializeField] private float meteorInitialSpeed = 5;
+
 
     [SerializeField][Range(0.1f, 20)] private float spawnFreq;
     private float spawnTime;
     [SerializeField] private GameObject meteor;
     private float spawnAngle, spawnPos;
+
+    void SpawnMeteor()
+    {
+        float spawnAngle = UnityEngine.Random.value * 2 * 3.14159f;  //Get random angle in radians
+
+        Vector2 generationDirection = new Vector2(Mathf.Cos(spawnAngle),Mathf.Sin(spawnAngle));
+        generationDirection *= spawnDistance;
+
+        float moveAngle = spawnAngle + 3.14159f;    //Rotate spawn angle by PI radians (180 degrees) to get direction of ship
+
+        //Generate random number between 0 -> 1, rescale to be between -1 -> 1, then multiply by randomOffsetRange to be between -randomOffsetRange -> randomOffsetRange, then convert to radians
+        float offset = (UnityEngine.Random.value * 2 - 1) * randomOffsetRange  * 3.14159f / 180;
+        moveAngle += offset;
+
+        Vector2 moveDirection = new Vector2(Mathf.Cos(moveAngle), Mathf.Sin(moveAngle)) * meteorInitialSpeed;
+
+        GameObject newMeteor = Instantiate(meteor);
+        newMeteor.transform.position = generationDirection;
+        newMeteor.GetComponent<Rigidbody2D>().velocity = moveDirection;
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +47,6 @@ public class MeteorSpawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawnTime += Time.deltaTime;
-        if (spawnTime > spawnFreq) {
-            spawnTime -= spawnFreq;
 
-            //spawnAngle = some random thingy(0, 2 * math.PI);
-
-
-            Instantiate(meteor); //Need to have position and velocity as parameters
-        }
     }
 }
