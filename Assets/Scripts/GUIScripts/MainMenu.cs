4bb288e54fd692bb2449d2dd7a739a08a601bb49
Rebecca <146950501+Azaersyss@users.noreply.github.com>
2024-11-03 03:14:23 +0000

    Buncha Last Minute Stuff ngl
    Added a main menu with functional play/quit buttons, GameScene, which will be the primary scene for the build, a temp background to the main menu and GameScene, among various audio tweaks and code.

---
+++ Assets/Scripts/GUIScripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        //Plays the States_Music event
+        AkSoundEngine.PostEvent("States_Music", this.gameObject);
+        //Sets the "Music" State Group's active State to "Menu"
+        AkSoundEngine.SetState("Music", "Menu");
+    }
+
+    public void PlayGame()
+    {
+        //Plays the Button_Click event
+        AkSoundEngine.PostEvent("Button_Click", this.gameObject);
+        //Sets the "Music" State Group's active State to "Gameplay"
+        AkSoundEngine.SetState("Music", "Gameplay");
+
+        SceneManager.LoadScene("GameScene");
+    }
+    public void BecTest()
+    {
+        //Plays the Button_Click event
+        AkSoundEngine.PostEvent("Button_Click", this.gameObject);
+
+        SceneManager.LoadScene("BecTestingScene");
+    }
+
+    public void QuitGame()
+    {
+        //Plays the Button_Click event
+        AkSoundEngine.PostEvent("Button_Click", this.gameObject);
+
+        Application.Quit();
+    }
+}
